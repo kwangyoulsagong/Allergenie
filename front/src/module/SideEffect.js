596e507a9c3b03ed5f3ba6379b5a297de6ad471e
kwangyoulsagong <sgky0511@naver.com>
2024-01-30 16:02:05 +0900

    feat: 부작용 이미지 추가 및 구현

--- front/src/module/SideEffect.js
+++ front/src/module/SideEffect.js
@@ -3,29 +3,60 @@ import Sleepy from "../img/sleep.svg";
 import Stomach from "../img/stomach.svg";
 import Vomit from "../img/vomit.svg";
 import Eyeloss from "../img/Eyeloss.svg";
+import Boktong from "../img/boktong.svg";
+import Headache from "../img/headache.svg";
+import Dizzy from "../img/dizzy.svg";
+import Sulsa from "../img/sulsa.svg";
+import HungryLess from "../img/hungryles.svg";
+import Heart from "../img/heart.svg";
+import Insomnia from "../img/insomnia.svg";
+import Honsu from "../img/honsu.svg";
+import Breath from "../img/breath.svg";
+import Depress from "../img/depress.svg";
+import Amnesia from "../img/amnesia.svg";
+import Soha from "../img/soha.svg";
 import { useLocation } from "react-router-dom";
 const SideEffect = () => {
   const location = useLocation();
-  const { searchResults } = location.state || {};
+  const { searchResults, sideEffectResult } = location.state || {};
+  const sideEffectImages = {
+    졸음: Sleepy,
+    위장장애: Stomach,
+    복통: Boktong,
+    구토: Vomit,
+    충혈: Eyeloss,
+    두통: Headache,
+    어지러움: Dizzy,
+    설사: Sulsa,
+    식욕부진: HungryLess,
+    "심혈관 질환": Heart,
+    불면증: Insomnia,
+    혼수: Honsu,
+    호흡곤란: Breath,
+    우울증: Depress,
+    "기억력 저하": Amnesia,
+    소화불량: Soha,
+  };
+
   return (
     <div>
       <div className="SideEffect-Container">
         <h1 className="sideEffectText">부작용</h1>
         <div className="sideEffectImgContainer">
-          <img src={Sleepy}></img>
-          <img src={Stomach}></img>
-          <img src={Vomit}></img>
-          <img src={Eyeloss}></img>
+          {sideEffectResult.sideEffect.map((value, index) => (
+            <img
+              key={index}
+              src={sideEffectImages[value.name]}
+              alt={value.name}
+            />
+          ))}
         </div>
         <div className="sideEffectTypeContainer">
-          <div className="sideEffectType">복통</div>
-          <div className="sideEffectType">복통</div>
-          <div className="sideEffectType">복통</div>
-          <div className="sideEffectType">복통</div>
-        </div>
-        <div className="sideEffectInfo">
-          약물의 종류에 따라 정도가 다르지만 대표적인 부작용은 졸음, 진정작용과
-          같은 중추신경계 부작용이다.
+          {sideEffectResult.sideEffect.map((value, index) => (
+            <div key={index} className="sideEffectType">
+              {value.name}
+            </div>
+          ))}
         </div>
         <h1 className="sideEffectWarning">주의사항 & 해결방안</h1>
         {searchResults.map((value, index) => (
